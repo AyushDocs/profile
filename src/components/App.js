ea361bb304c4e9b2d9267eeb2340596e4e4d6daf
ayush <notTelling@gmail.com>
2022-01-08 18:41:53 +0530

    created project item, currently using dummy data, will get live data later

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
+import Project from './Project';
 
 const App = () => {
 	return (
 		<div>
 			<Routes>
 				<Route path='/' element={<></>} />
-				<Route path='/projects' element={<></>} />
+				<Route path='/projects' element={<Project/>} />
 			</Routes>
 		</div>
 	);
